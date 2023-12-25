421a6ef916352d67c1c1b75774b04ed49ab4cbad
gautam2378 <gautam672001@gmail.com>
2023-12-25 10:22:52 +0530

    Final Edit works

--- app.py
+++ app.py
@@ -1,38 +1,103 @@
-from flask import Flask, jsonify
-from flask import Flask,request
+# from flask import Flask, jsonify
+# from flask import Flask,request
+# from flask_cors import CORS, cross_origin
+# import pandas as pd
+# import json
+
+# app = Flask(__name__)
+# cors = CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
+
+# @app.route('/getData',methods=['GET'])
+# @cross_origin()
+# def get_data():
+#     df = pd.read_excel("Pivot Practice.xlsx",sheet_name="Sheet5")
+#     row = ['Country', 'Gender', 'Age-Group']
+#     column = ['Year']
+#     value = 'People'
+#     pivot_table = pd.pivot_table(df, values=value, index=row, columns=column, aggfunc='sum', fill_value=0, margins=True, margins_name='Total', observed=True)
+#     # Implement logic to fetch and process data
+#     # Return the data in JSON format
+#     np_array = pivot_table.to_numpy().tolist()
+#     print(np_array)
+#     # return jsonify({'pivot':np_array})
+#     return jsonify({'pivot': np_array})
+# @app.route('/',methods=['GET'])
+# @cross_origin()
+# def get_pt():
+#     df = pd.read_excel("Pivot Practice.xlsx",sheet_name="Sheet5")
+#     row = ['Country', 'Gender', 'Age-Group']
+#     column = ['Year']
+#     value = 'People'
+#     pivot_table = pd.pivot_table(df, values=value, index=row, columns=column, aggfunc='sum', fill_value=0, margins=True, margins_name='Total', observed=True)
+#     return jsonify(pivot_table.to_dict(orient='split'))
+  
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True,port=8080)
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 import pandas as pd
-import json
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/getData',methods=['GET'])
+# Load the initial data
+df = pd.read_excel("Pivot Practice.xlsx", sheet_name="Sheet5")
+row = ['Country', 'Gender', 'Age-Group']
+column = ['Year']
+value = 'People'
+pivot_table = pd.pivot_table(
+    df,
+    values=value,
+    index=row,
+    columns=column,
+    aggfunc='sum',
+    fill_value=0,
+    margins=True,
+    margins_name='Total',
+    observed=True
+)
+
+@app.route('/getData', methods=['GET'])
 @cross_origin()
 def get_data():
-    df = pd.read_excel("Pivot Practice.xlsx",sheet_name="Sheet5")
-    row = ['Country', 'Gender', 'Age-Group']
-    column = ['Year']
-    value = 'People'
-    pivot_table = pd.pivot_table(df, values=value, index=row, columns=column, aggfunc='sum', fill_value=0, margins=True, margins_name='Total', observed=True)
-    # Implement logic to fetch and process data
-    # Return the data in JSON format
     np_array = pivot_table.to_numpy().tolist()
-    print(np_array)
-    # return jsonify({'pivot':np_array})
     return jsonify({'pivot': np_array})
-@app.route('/',methods=['GET'])
+
+@app.route('/updateData', methods=['POST'])
+@cross_origin()
+def update_data():
+    data = request.get_json()
+    i, j, val = data['i'], data['j'], data['val']
+    
+    if i >= 0 and j >= 0 and i < len(pivot_table.index) and j < len(pivot_table.columns):
+        pivot_table.iloc[i, j] = val
+        # Save changes to the Excel file
+        pivot_table.to_excel("Pivot Practice.xlsx", sheet_name="Sheet5", index=False)
+        return jsonify({'success': True})
+    else:
+        return jsonify({'success': False, 'message': 'Invalid indices'})
+
+@app.route('/saveChanges', methods=['POST'])
+@cross_origin()
+def save_changes():
+    data = request.get_json()
+    # Process and save changes as needed
+    # Example: You might update the DataFrame with the provided data
+    # and then save it to the Excel file
+    # Note: Make sure to validate and handle data appropriately
+    updated_data = data.get('updatedData')
+    # ... Process and save changes ...
+    
+    return jsonify({'success': True, 'message': 'Changes saved successfully'})
+
+@app.route('/', methods=['GET'])
 @cross_origin()
 def get_pt():
-    df = pd.read_excel("Pivot Practice.xlsx",sheet_name="Sheet5")
-    row = ['Country', 'Gender', 'Age-Group']
-    column = ['Year']
-    value = 'People'
-    pivot_table = pd.pivot_table(df, values=value, index=row, columns=column, aggfunc='sum', fill_value=0, margins=True, margins_name='Total', observed=True)
     return jsonify(pivot_table.to_dict(orient='split'))
-  
-
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8080)
+    app.run(debug=True, port=8080)
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,7 +1,191 @@
+// import './App.css';
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+
+// function App() {
+//   const [pivotTable, setPivotTable] = useState([]);
+//   const [updateParams, setUpdateParams] = useState({
+//     columns: [],
+//     data: [],
+//     index: [],
+//     i: -1,
+//     j: -1,
+//     val: 0,
+//   });
+//   const [isEditing, setIsEditing] = useState(false);
+
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+// //   const fetchData = async () => {
+// //     try {
+// //       const response = await axios.get('http://127.0.0.1:8080');
+// //       setPivotTable(response.data);
+// //       setUpdateParams({
+// //         ...updateParams,
+// //         columns: response.data.columns,
+// //         data: response.data.data,
+// //         index: response.data.index,
+// //       });
+// //     } catch (error) {
+// //       console.log(error);
+// //     }
+// //   };
+
+//   const fetchData = async () => {
+//     try {
+//       const response = await axios.get('http://127.0.0.1:8080');
+//       setPivotTable(response.data);
+//       setUpdateParams({
+//         ...updateParams,
+//         columns: response.data.columns,
+//         data: response.data.data,
+//         index: response.data.index,
+//       });
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+// //   const handleUpdateData = async () => {
+// //     try {
+// //       await axios.post('http://127.0.0.1:8080/updateData', updateParams);
+// //       // Refresh data after updating
+// //       fetchData();
+// //       // Reset updateParams to default values
+// //       setUpdateParams({ columns: [], data: [], index: [], i: -1, j: -1, val: 0 });
+// //     } catch (error) {
+// //       console.error('Error updating data:', error);
+// //     }
+// //   };
 
 
+//   // const handleUpdateData = async () => {
+//   //   try {
+//   //     await axios.post('http://127.0.0.1:8080/updateData', updateParams);
+   
+//   //     fetchData();
+      
+//   //     setUpdateParams({ columns: [], data: [], index: [], i: -1, j: -1, val: 0 });
+      
+//   //     setIsEditing(false);
+//   //   } catch (error) {
+//   //     console.error('Error updating data:', error);
+//   //   }
+//   // };
 
+//   // const handleEditClick = () => {
+   
+//   //   setIsEditing((prevIsEditing) => !prevIsEditing);
+//   // };
+//   const handleCellEdit = (e, rowIndex, colIndex) => {
+//     const updatedData = [...updateParams.data];
+//     updatedData[rowIndex][colIndex] = e.target.innerText; // Use innerText for contentEditable
+  
+//     setUpdateParams({
+//       ...updateParams,
+//       data: updatedData,
+//     });
+//   };
+//   return (
+//     <div className="App">
+//       <h1>Forecasting App</h1>
 
+//       <div>
+//         <h2>Pivot Table</h2>
+       
+//         <table>
+//           <thead>
+//             <tr>
+//               <th>Country</th>
+//               <th>Gender</th>
+//               <th>Age-Group</th>
+//               {updateParams.columns.map((year, index) => (
+//                 <th key={index}>{year}</th>
+//               ))}
+//             </tr>
+//           </thead>
+//           <tbody>
+//           {updateParams.index.map((rowData, rowIndex) => (
+//   <tr key={rowIndex}>
+//     {rowData.map((value, colIndex) => (
+//       <td
+//         key={colIndex}
+//         contentEditable={isEditing}
+//         suppressContentEditableWarning
+//         onBlur={(e) => handleCellEdit(e, rowIndex, colIndex)}
+//       >
+//         {value}
+//       </td>
+//     ))}
+//     {updateParams.data[rowIndex].map((cellValue, colIndex) => (
+//       <td
+//         key={colIndex}
+//         contentEditable={isEditing}
+//         suppressContentEditableWarning
+//         onBlur={(e) => handleCellEdit(e, rowIndex, colIndex)}
+//       >
+//         {cellValue}
+//       </td>
+//     ))}
+//   </tr>
+// ))}
+//             {/* {updateParams.index.map((rowData, rowIndex) => (
+//               <tr key={rowIndex}>
+//                 {rowData.map((value, colIndex) => (
+//                   <td key={colIndex} contentEditable={isEditing} suppressContentEditableWarning>
+//                     {value}
+//                   </td>
+//                 ))}
+                
+//                 {updateParams.data[rowIndex].map((cellValue, colIndex) => (
+//                   <td key={colIndex} contentEditable={isEditing} suppressContentEditableWarning>
+//                     {cellValue}
+//                   </td>
+//                 ))}
+//               </tr>
+//             ))} */}
+//           </tbody>
+//         </table>
+        
+//       </div>
+// <div >
+// <button onClick={handleCellEdit}>{isEditing ? 'Update' : 'Edit'}</button>
+// </div>
+//       {/* <div>
+//         <h2>Update Data</h2>
+//         <label>
+//           i:
+//           <input
+//             type="number"
+//             value={updateParams.i}
+//             onChange={(e) => setUpdateParams({ ...updateParams, i: parseInt(e.target.value) })}
+//           />
+//         </label>
+//         <label>
+//           j:
+//           <input
+//             type="number"
+//             value={updateParams.j}
+//             onChange={(e) => setUpdateParams({ ...updateParams, j: parseInt(e.target.value) })}
+//           />
+//         </label>
+//         <label>
+//           val:
+//           <input
+//             type="number"
+//             value={updateParams.val}
+//             onChange={(e) => setUpdateParams({ ...updateParams, val: parseInt(e.target.value) })}
+//           />
+//         </label>
+//         <button onClick={handleUpdateData}>Update Data</button>
+//       </div> */}
+//     </div>
+//   );
+// }
+
+// export default App;
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
@@ -22,21 +206,6 @@ function App() {
     fetchData();
   }, []);
 
-//   const fetchData = async () => {
-//     try {
-//       const response = await axios.get('http://127.0.0.1:8080');
-//       setPivotTable(response.data);
-//       setUpdateParams({
-//         ...updateParams,
-//         columns: response.data.columns,
-//         data: response.data.data,
-//         index: response.data.index,
-//       });
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-
   const fetchData = async () => {
     try {
       const response = await axios.get('http://127.0.0.1:8080');
@@ -52,35 +221,35 @@ function App() {
     }
   };
 
-//   const handleUpdateData = async () => {
-//     try {
-//       await axios.post('http://127.0.0.1:8080/updateData', updateParams);
-//       // Refresh data after updating
-//       fetchData();
-//       // Reset updateParams to default values
-//       setUpdateParams({ columns: [], data: [], index: [], i: -1, j: -1, val: 0 });
-//     } catch (error) {
-//       console.error('Error updating data:', error);
-//     }
-//   };
+  const handleCellEdit = (e, rowIndex, colIndex) => {
+    const updatedData = [...updateParams.data];
+    updatedData[rowIndex][colIndex] = e.target.innerText; 
+
+    setUpdateParams({
+      ...updateParams,
+      data: updatedData,
+      i: rowIndex,
+      j: colIndex,
+      val: e.target.innerText,
+    });
+  };
+
+  const handleEditClick = () => {
+    setIsEditing((prevIsEditing) => !prevIsEditing);
+  };
 
   const handleUpdateData = async () => {
     try {
       await axios.post('http://127.0.0.1:8080/updateData', updateParams);
-   
+
       fetchData();
-      
+
       setUpdateParams({ columns: [], data: [], index: [], i: -1, j: -1, val: 0 });
-      
+
       setIsEditing(false);
     } catch (error) {
       console.error('Error updating data:', error);
     }
-  };
-
-  const handleEditClick = () => {
-   
-    setIsEditing((prevIsEditing) => !prevIsEditing);
   };
 
   return (
@@ -89,7 +258,7 @@ function App() {
 
       <div>
         <h2>Pivot Table</h2>
-       
+
         <table>
           <thead>
             <tr>
@@ -102,17 +271,25 @@ function App() {
             </tr>
           </thead>
           <tbody>
-          
             {updateParams.index.map((rowData, rowIndex) => (
               <tr key={rowIndex}>
                 {rowData.map((value, colIndex) => (
-                  <td key={colIndex} contentEditable={isEditing} suppressContentEditableWarning>
+                  <td
+                    key={colIndex}
+                    contentEditable={isEditing}
+                    suppressContentEditableWarning
+                    onBlur={(e) => handleCellEdit(e, rowIndex, colIndex)}
+                  >
                     {value}
                   </td>
                 ))}
-                
                 {updateParams.data[rowIndex].map((cellValue, colIndex) => (
-                  <td key={colIndex} contentEditable={isEditing} suppressContentEditableWarning>
+                  <td
+                    key={colIndex}
+                    contentEditable={isEditing}
+                    suppressContentEditableWarning
+                    onBlur={(e) => handleCellEdit(e, rowIndex, colIndex)}
+                  >
                     {cellValue}
                   </td>
                 ))}
@@ -120,43 +297,15 @@ function App() {
             ))}
           </tbody>
         </table>
-        
       </div>
-<div>
-<button onClick={handleEditClick}>{isEditing ? 'Update' : 'Edit'}</button>
-</div>
-      {/* <div>
-        <h2>Update Data</h2>
-        <label>
-          i:
-          <input
-            type="number"
-            value={updateParams.i}
-            onChange={(e) => setUpdateParams({ ...updateParams, i: parseInt(e.target.value) })}
-          />
-        </label>
-        <label>
-          j:
-          <input
-            type="number"
-            value={updateParams.j}
-            onChange={(e) => setUpdateParams({ ...updateParams, j: parseInt(e.target.value) })}
-          />
-        </label>
-        <label>
-          val:
-          <input
-            type="number"
-            value={updateParams.val}
-            onChange={(e) => setUpdateParams({ ...updateParams, val: parseInt(e.target.value) })}
-          />
-        </label>
-        <button onClick={handleUpdateData}>Update Data</button>
-      </div> */}
+
+      <div>
+        <button onClick={isEditing ? handleUpdateData : handleEditClick}>
+          {isEditing ? 'Update' : 'Edit'}
+        </button>
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
